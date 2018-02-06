459b2e72d1fb1cd150ed3cd12860727002a6812a
MatiasCoiman <36193664+MatiasCoiman@users.noreply.github.com>
2018-02-06 11:50:19 -0300

    ejercicio 3,4,5
    Tomar por id

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -1,7 +1,12 @@
 /*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
-{	
+{	var nombre;
+	nombre=document.getElementById('elNombre').value;  //primero se hace la variable , despues el id y por ultimo la alerta.
+
+	var edad;
+	edad=document.getElementById('laEdad').value;
+	alert("Usted se llama "+ nombre+" y tiene "+edad+" años" );
 	
 }
 
