677eee02d72c26ecf03917aee16fa63d4791577c
MatiasCoiman <36193664+MatiasCoiman@users.noreply.github.com>
2018-02-07 12:14:31 -0300

    Ejercicio 1,2 y 3.
    If, else ,&&, ||.

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,31 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad 
+	
+	var edad;
+
+	edad=document.getElementById('edad').value;
+
+	if(edad<17)
+		{ alert("menor")
+	}
+
+	else
+				{ alert("mayor")}
+
+
+	/*var edad;
+
+	edad=document.getElementById('edad').value;
+
+	if(edad==17)	
+		{ alert("Eres menor de edad")
+	}
+
+	if(edad==18)
+		{ alert("Eres mayor de edad")
+    }
+*/
 
 
 }//FIN DE LA FUNCIÓN
