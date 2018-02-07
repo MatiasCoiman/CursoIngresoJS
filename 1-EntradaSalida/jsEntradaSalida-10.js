507153e99651dd2a3ba593505f1230fd7765ba56
MatiasCoiman <36193664+MatiasCoiman@users.noreply.github.com>
2018-02-07 10:56:21 -0300

    ejercicio 6,7,8,9y 10.

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,22 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+	var resultado;
+	var descuento;
+
+	importe=document.getElementById('importe').value;
+
+	importe=parseInt(importe);
+
+	descuento=importe*0.75;
+
+	resultado=descuento;
+
+	document.getElementById('resultado').value=resultado;
+
+
+
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,7 +2,26 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
-{
+{  /*var numeroUno;
+   var numeroDos;
+	document.getElementById('numeroUno').value;   //el + sirve para concatenar y para sumar, si tiene comillas no se podra sumar, se necesita parseInt para poder sumar y tambien sirve para transformarlos en numeros enteros.
+	document.getElementById('numeroDos').value;
+
+ */	
+ 	var resultado;
+ 	var numeroUno;
+ 	var numeroDos;
+
+ 	numeroUno=document.getElementById('numeroUno').value;
+ 	numeroDos=document.getElementById('numeroDos').value;
+ 	
+ 	numeroUno=parseInt(numeroUno);
+ 	numeroDos=parseInt(numeroDos);
+
+
+ 	resultado=numeroUno+numeroDos;
+
+ 	alert("El suma es "+resultado);
 
 }
 
