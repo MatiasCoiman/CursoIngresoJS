fb3d8c2e051c9146571811aa8469482d295a5f19
MatiasCoiman <36193664+MatiasCoiman@users.noreply.github.com>
2018-02-13 20:29:02 -0300

    Ejercicio 4,5 y 6 del if.

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,16 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+
+	edad=document.getElementById('edad').value;
+
+	if(edad>13&&edad<17);
+	
+	else
+			{alert("No son adolescentes");
+			}	
+		
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+
+	edad=document.getElementById('edad').value;
+
+	if(edad>=18)
+		{alert("Mayor de edad")}
+
+	if(edad<18&&edad>=13)
+		{alert("Adolescentes")}
+	
+	if(edad<13)
+		{alert("Niños")}
+
+
+
 
 
 
