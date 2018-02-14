f917d405e0247e512d5d9121e2eaefce789d0c6a
MatiasCoiman <36193664+MatiasCoiman@users.noreply.github.com>
2018-02-14 10:35:45 -0300

    Ejercicio 7,8,9 y 10
    numero random

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,34 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numeroAleatorio;
+	numeroAleatorio=Math.floor((Math.random() * 10) + 1);
+/*
+	if(numeroAleatorio==9 || numeroAleatorio==10)
+	 	{ alert(numeroAleatorio+" EXCLENTE");
+	 	}
+	if(numeroAleatorio>=4 && numeroAleatorio<=8)
+	 	{ alert(numeroAleatorio+" APROBO");
+	 	}
+	if(numeroAleatorio<4)
+	 	{ alert(numeroAleatorio+" Vamos ,la proxima se puede");
+	 	}
+*/
+	if(numeroAleatorio>8)
+	{
+		alert("excelente");
+	}
+	else
+		{	if(numeroAleatorio>3&&numeroAleatorio<9)
+				{ alert("aprobo");
+			else
+			{
+				alert("desaprobo");
+			}
+	}
+		
 	
+
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,6 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+	var estadoCivil;
+	//console.log("hola");
+	edad=document.getElementById('edad').value;
+	estadoCivil=document.getElementById('estadoCivil').value;
+
+	if(estadoCivil!="Soltero" && edad<18)
+	{ console.log("NO HACER NADA");
+	}
+	else
+		{ if(estadoCivil=="Soltero" && edad>=18)
+			alert("Es soltero y no es menor");	
+
+		}
+	
+
 
 	
 
