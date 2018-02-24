59fe8f99e9e8529304f796eb42866120fc2c4d91
MatiasCoiman <36193664+MatiasCoiman@users.noreply.github.com>
2018-02-24 12:21:20 -0300

    ejercicio 8 sin terminar parte 2.

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -7,25 +7,52 @@ function Mostrar()
 	var nota;
 	var promedio;
 	var acumulador=0;
+	var sexo;
+	var sexoF=0;
+	var sexoM=0;
+	var desaprobados=0;
 	while(respuesta!="no")
 		{
-			alumno=prompt("ingrese nombre de alumno");
-			cantidad=cantidad+1;
-			nota=prompt("ingrese la nota");
-			nota=parseInt(nota);
-			while(nota<0 || nota>10)
+				alumno=prompt("ingrese nombre de alumno");
+				cantidad=cantidad+1;
+				nota=prompt("ingrese la nota");
+				nota=parseInt(nota);
+		while(nota<0 || nota>10)
 				{
-					nota=prompt("ingrese la nota");
-					nota=parseInt(nota);	
+				nota=prompt("ingrese la nota");
+				nota=parseInt(nota);	
 				}	
-
-				acumulador=acumulador+nota;
+				sexo=prompt("ingrese f ó m .");
+		while(!(sexo=="f" || sexo=="m"))
+				{
+					sexo = prompt("error,ingrese f ó m .");
+				}	
+				if(sexo=="f")
+				{	
+					sexoF++;
+			
+				}	
+				if(sexo=="m")
+				{	
+				sexoM++;
+		
+				}
+	
+			acumulador=acumulador+nota;
 
 			promedio=acumulador/cantidad;
 			respuesta=prompt("¿quieres ingresar otro alumno?");
-		}
-document.write("Se ingreso "+cantidad+" alumnos" + " y el promedio total es "+promedio);
 
+		if(sexo=="m" && nota<4)
+			{
+			desaprobados++;
+			desprobados=parseInt(desaprobados);
+			}	
+		}
+
+document.write("Se ingreso "+cantidad+" alumnos" + " y el promedio total es "+promedio);
+document.write("<br>" + "Hay "+sexoF+ " personas femeninas "+ " y " +sexoM + " personas masculinas");
+document.write("<br>" + "hay "+desaprobados+ " desaprobado");
 
 
 }
