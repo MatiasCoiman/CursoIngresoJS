b04d5f7fa2793d878d6458b02030daa5bf79c5b4
MatiasCoiman <36193664+MatiasCoiman@users.noreply.github.com>
2018-02-24 10:50:16 -0300

    Ejercicio 8 sin terminar.

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,4 +1,31 @@
+//ingreso de alumnos, no sabemos cuantos alumnos hay, vamos a pedir el nombre , quiero saber cuantos alumnos ingrese
 function Mostrar()
 {
+	var alumno;
+	var cantidad=0;
+	var respuesta;
+	var nota;
+	var promedio;
+	var acumulador=0;
+	while(respuesta!="no")
+		{
+			alumno=prompt("ingrese nombre de alumno");
+			cantidad=cantidad+1;
+			nota=prompt("ingrese la nota");
+			nota=parseInt(nota);
+			while(nota<0 || nota>10)
+				{
+					nota=prompt("ingrese la nota");
+					nota=parseInt(nota);	
+				}	
+
+				acumulador=acumulador+nota;
+
+			promedio=acumulador/cantidad;
+			respuesta=prompt("¿quieres ingresar otro alumno?");
+		}
+document.write("Se ingreso "+cantidad+" alumnos" + " y el promedio total es "+promedio);
+
+
 
 }
