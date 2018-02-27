9767ba7ed53ab04d579c66c87185f8a4526d2fe3
MatiasCoiman <36193664+MatiasCoiman@users.noreply.github.com>
2018-02-27 12:23:53 -0300

    ejercicio 6 y 7 en clase.
    ejercicio 6 y 7 en clase.

--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,69 @@
 function Mostrar()
 {
+	var numero;
+	var numerosPositivos;
+	var respuesta="si";
+	var cantidadPositivo=0;
+	var cantidadNumeroPar=0;
+	var numeroImpar=0;
+	var promedioPositivo;
+	var maximo=-1000;
+	var minimo=1000;
+	var acumuladorPositivo=0;
+	var contador=00
+	while(respuesta!="no")
+	{
+		contador++;
+		numero=prompt("ingrese un numero entre -50 y 50");
+		numero=parseInt(numero);
+		
+		
 
+		respuesta=prompt("Ingrese no,para salir");
+
+		while(numero<-50 || numero>50 || isNaN(numero))
+		{
+			numero=prompt("Error ingrese un numero valido");
+		}
+		if(numero%2==0)
+		{
+			cantidadNumeroPar++;
+		}
+		else
+		{
+			numeroImpar++;
+		}
+		if(numero==1)
+		{
+			maximo=numero;
+			minimo=numero;
+		}
+		else
+		{
+			if(numero>maximo)
+			{
+				maximo=numero;
+			}
+			if(numero<minimo)
+			{
+				minimo=numero;
+			}
+		}
+		if(numero>0)
+		{
+			cantidadPositivo++;
+			acumuladorPositivo=acumuladorPositivo+numero;
+			promedioPositivo=acumuladorPositivo/cantidadPositivo;
+
+		}		
+
+	}
+
+	
+
+	document.write("<br>" +"Numeros pares "+cantidadNumeroPar);
+	document.write("<br> " + "Numero impares "+numeroImpar);
+	document.write("<br> "+ "Promedio positivo "+promedioPositivo);
+	document.write("<br> "+ "Numero maximo "+maximo);
+	document.write("<br> "+ "Numero minimo "+minimo);
 }
--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -1,4 +1,78 @@
 function Mostrar()
 {
+	var letra;
+	var numero;
+	var minimo=999;
+	var maximo=-9999;
+	var promedio;
+	var respuesta="si";
+	var contador=0;
+	var letraMax;
+	var letraMin;
+	var numeroVocal=0;
+	var acumuladorVocal=0;
 
+	while(respuesta!="no")
+	{
+		contador++;
+		letra=prompt("Ingrese una Letra");
+		while(!(isNaN(letra)))
+		{
+			letra=prompt("Error , ingrese una letra");
+
+		}
+		
+		numero=prompt("Ingrese un numero entre -200 y 200");
+		numero=parseInt(numero);
+		while(numero<-200 || numero>200)
+		{
+			numero=prompt("Error ingrese un numero valido");
+			numero=parseInt(numero);
+		}
+	
+		
+		
+		if(numero==1)
+		{
+			maximo=numero;
+			letraMax=letra;
+			minimo=numero;
+			letraMin=letra;
+		}
+		else
+		{
+			if(numero>maximo)
+			{
+				maximo=numero;
+				letraMax=letra;
+			}
+			if(numero<minimo)
+			{
+				minimo=numero;
+				letraMin=letra;
+			}
+		
+			
+		}
+
+		switch(letra)
+			{
+				case"a":
+				case"e":
+				case"i":
+				case"o":
+				case"u":
+					numeroVocal++;
+					acumuladorVocal=acumuladorVocal+numero;
+					break;
+				
+					
+			}		
+		respuesta=prompt("Ingrese no,para salir");	
+	}
+	promedio=acumuladorVocal/numeroVocal;
+
+	document.write("<br> "+"La letra del numero Minimo es :"+letraMin);
+	document.write("<br> "+"La letra del numero Maximo es :"+letraMax);
+	document.write("<br> "+ "El promedio de los numeros despues de una vocal es :"+promedio);
 }
